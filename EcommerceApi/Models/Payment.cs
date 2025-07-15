db4fad59b3ced281b793426843344157eec6c972
PlaciDev <brunoplacidev@gmail.com>
2025-07-15 18:11:25 -0300

    cria o modelo Payment e define suas propriedades

---
+++ EcommerceApi/Models/Payment.cs
@@ -0,0 +1,29 @@
+﻿namespace EcommerceApi.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public PaymentMethod Method { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public PaymentStatus Status { get; set; }
+    }
+
+    public enum PaymentMethod
+    {
+        CreditCard,
+        DebitCard,
+        Pix
+
+    }
+
+    public enum PaymentStatus
+    {
+        Pending,
+        Completed,
+        Failed,
+        Refunded
+    }
+
+    
+}
