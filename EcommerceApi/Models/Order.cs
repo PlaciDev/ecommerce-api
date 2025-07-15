ba02874d7c74c4bf10b26bdac02738f76d0c1bd8
PlaciDev <brunoplacidev@gmail.com>
2025-07-15 18:09:43 -0300

    cria o modelo Order define suas propriedades

---
+++ EcommerceApi/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿namespace EcommerceApi.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public enum OrderStatus
+        {
+            Pending,
+            Processing,
+            Shipped,
+            Delivered,
+            Cancelled
+        }
+
+    }
+}
+        
