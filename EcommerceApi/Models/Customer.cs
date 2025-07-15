e4c5828cece83cd24d41da612efcdadd307b203f
PlaciDev <brunoplacidev@gmail.com>
2025-07-15 18:09:27 -0300

    cria o modelo Customer e define suas propriedades

---
+++ EcommerceApi/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace EcommerceApi.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public Address Address { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+
+
+    }
+}
