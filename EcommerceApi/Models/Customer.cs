5cf0599705aaa6cbcb57ed44ec68efad17e63292
PlaciDev <brunoplacidev@gmail.com>
2025-07-15 21:12:43 -0300

    define os relacionamentos de Customer

--- EcommerceApi/Models/Customer.cs
+++ EcommerceApi/Models/Customer.cs
@@ -3,12 +3,11 @@
     public class Customer
     {
         public int Id { get; set; }
-
-        public int UserId { get; set; }
-        public User User { get; set; }
         public Address Address { get; set; }
         public string PhoneNumber { get; set; }
 
+        public int UserId { get; set; }
+        public User User { get; set; }
         public ICollection<Order> Orders { get; set; } = new List<Order>();
 
 
