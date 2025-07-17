d5dde62d8885e79d2c420ad2f43341b2c5b1c77b
PlaciDev <brunoplacidev@gmail.com>
2025-07-17 17:55:49 -0300

    cria o controlador de Role e define os métodos CRUD

---
+++ EcommerceApi/Controllers/RoleController.cs
@@ -0,0 +1,135 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Controllers
+{
+    public class RoleController : ControllerBase
+    {
+
+        [HttpGet("api/roles")]
+        public async Task<IActionResult> Get(
+            [FromServices] ApiDbContext context)
+        {
+            try
+            {
+                var roles = await context
+                    .Roles
+                    .AsNoTracking()
+                    .ToListAsync();
+                if (roles is null || !roles.Any())
+                {
+                    return NotFound("Nenhum perfil encontrado...");
+                }
+                return Ok(roles);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno no servidor...");
+            }
+        }
+
+        [HttpGet("api/roles/{id:int}")]
+        public async Task<IActionResult> GetById(
+            [FromServices] ApiDbContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                var role = await context
+                    .Roles
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(x => x.Id == id);
+                if (role is null)
+                {
+                    return NotFound("Perfil não encontrado...");
+                }
+                return Ok(role);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno no servidor...");
+            }
+        }
+
+        [HttpPost("api/roles")]
+        public async Task<IActionResult> Post(
+            [FromServices] ApiDbContext context,
+            [FromBody] Role model)
+        {
+            try
+            {
+                var role = new Role
+                {
+                    Name = model.Name,
+                    Description = model.Description
+                };
+
+                await context.Roles.AddAsync(role);
+                await context.SaveChangesAsync();
+
+                return Created($"api/roles/{role.Id}", role);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno no servidor...");
+
+            }
+        }
+
+        [HttpPut("api/roles/{id:int}")]
+        public async Task<IActionResult> Put(
+            [FromServices] ApiDbContext context,
+            [FromRoute] int id,
+            [FromBody] Role model)
+        {
+            try
+            {
+                var role = await context.Roles.FirstOrDefaultAsync(x => x.Id == id);
+                if (role is null)
+                {
+                    return NotFound("Perfil não encontrado...");
+                }
+
+                role.Name = model.Name;
+                role.Description = model.Description;
+
+                context.Roles.Update(role);
+                await context.SaveChangesAsync();
+
+                return Ok(role);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno no servidor...");
+            }
+        }
+
+        [HttpDelete("api/roles/{id:int}")]
+        public async Task<IActionResult> Delete(
+            [FromServices] ApiDbContext context,
+            [FromRoute] int id)
+        {
+            try
+            {
+                var role = await context.Roles.FirstOrDefaultAsync(x => x.Id == id);
+                if (role is null)
+                {
+                    return NotFound("Perfil não encontrado...");
+                }
+
+                context.Roles.Remove(role);
+                await context.SaveChangesAsync();
+
+                return Ok("Perfil removido com sucesso...");
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno no servidor...");
+            }
+        }
+
+
+    }
+}
