40fe75551df8119a27d4a5395e2a86667b5923c5
PlaciDev <brunoplacidev@gmail.com>
2025-07-17 17:54:20 -0300

    ajusta as configuraçõe do banco, adicionando o serviço em Program e realizando uma nova migração

---
+++ EcommerceApi/Migrations/20250717203806_CorrectsMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceApi.Migrations
+{
+    public partial class CorrectsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -1,8 +1,14 @@
+using EcommerceApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApiDbContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
