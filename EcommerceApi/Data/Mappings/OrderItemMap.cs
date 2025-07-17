a0306cd074a74aa7fe18a02751560e10ea1ae2d0
PlaciDev <brunoplacidev@gmail.com>
2025-07-16 22:20:04 -0300

    configura o mapeamento de OrderItem

---
+++ EcommerceApi/Data/Mappings/OrderItemMap.cs
@@ -0,0 +1,39 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Data.Mappings
+{
+    public class OrderItemMap : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItem");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Quantity)
+                .IsRequired()
+                .HasColumnName("Quantity")
+                .HasColumnType("INTEGER");
+
+            builder.Property(x => x.UnitPrice)
+                .IsRequired()
+                .HasColumnName("UnitPrice")
+                .HasColumnType("DECIMAL(18,2)");
+
+            builder.HasOne(x => x.Order)
+                .WithMany(x => x.OrderItems)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Product)
+                .WithMany(x => x.OrderItems)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+        }
+    }
+}
