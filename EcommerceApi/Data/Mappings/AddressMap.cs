c9a9e5584d844dfdde45e2cb9752f7ae2099f636
PlaciDev <brunoplacidev@gmail.com>
2025-07-16 22:19:29 -0300

    configura o mapeamento de Address

---
+++ EcommerceApi/Data/Mappings/AddressMap.cs
@@ -0,0 +1,53 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Data.Mappings
+{
+    public class AddressMap : IEntityTypeConfiguration<Address>
+    {
+        public void Configure(EntityTypeBuilder<Address> builder)
+        {
+            builder.ToTable("Address");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Street)
+                .IsRequired()
+                .HasColumnName("Street")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.City)
+                .IsRequired()
+                .HasColumnName("City")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.State)
+                .IsRequired()
+                .HasColumnName("State")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+             builder.Property(x => x.ZipCode)
+                .IsRequired()
+                .HasColumnName("ZipCode")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(20);
+
+            builder.HasOne(x => x.Customer)
+                .WithMany(x => x.Addresses)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+
+
+
+        }
+    }
+}
