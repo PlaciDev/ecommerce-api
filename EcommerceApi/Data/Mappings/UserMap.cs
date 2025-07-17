c84c6dba5adb4861c3827ced6dd4b2605739e853
PlaciDev <brunoplacidev@gmail.com>
2025-07-16 22:20:47 -0300

    configura o mapeamento de User

---
+++ EcommerceApi/Data/Mappings/UserMap.cs
@@ -0,0 +1,55 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasColumnName("Email")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired()
+                .HasColumnName("PasswordHash")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(255);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired()
+                .HasColumnName("CreatedAt")
+                .HasColumnType("DATETIME");
+
+            builder.HasOne(x => x.Role)
+                .WithMany(x => x.Users)
+                .HasForeignKey("RoleId")
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.CustomerProfile)
+                .WithOne(x => x.User)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+
+        }
+    }
+}
