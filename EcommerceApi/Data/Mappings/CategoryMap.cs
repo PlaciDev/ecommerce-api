9d6779055e4db6279c95f6296b184416713f9dc9
PlaciDev <brunoplacidev@gmail.com>
2025-07-16 22:19:41 -0300

    configura o mapeamento de Category

---
+++ EcommerceApi/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,35 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.ToTable("Category");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(20);
+
+            builder.Property(x => x.Description)
+                .IsRequired()
+                .HasColumnName("Description")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.HasMany(x => x.Products)
+                .WithOne(x => x.Category)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
