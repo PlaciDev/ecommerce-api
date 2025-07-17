e3147c40c41a08ab6e53f9391a48feb0d5096aef
PlaciDev <brunoplacidev@gmail.com>
2025-07-17 17:30:21 -0300

    adiciona uma lista de OrdeItems no modelo Product

--- EcommerceApi/Models/Product.cs
+++ EcommerceApi/Models/Product.cs
@@ -10,6 +10,7 @@
 
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
     }
 }
