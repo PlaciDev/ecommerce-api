c852c97ddc783dfe0cabf3de5f15176dd257c1f9
PlaciDev <brunoplacidev@gmail.com>
2025-07-16 22:20:40 -0300

    configura o mapeamento de Role

---
+++ EcommerceApi/Data/Mappings/RoleMap.cs
@@ -0,0 +1,36 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Data.Mappings
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Role");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn();
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(20);
+
+            builder.Property(x => x.Description)
+                .IsRequired()
+                .HasColumnName("Description")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(100);
+
+            builder.HasMany(x => x.Users)
+                .WithOne(x => x.Role)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
