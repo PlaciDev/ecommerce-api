693a52b5e614398eacac7c5e0e6843240c148d49
PlaciDev <brunoplacidev@gmail.com>
2025-07-20 18:50:19 -0300

    cria os ViewModels de listagem e edição, para o controlador User

---
+++ EcommerceApi/ViewModel/UserListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceApi.ViewModel
+{
+    public class UserListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public RoleListViewModel Role { get; set; }
+    }
+}
