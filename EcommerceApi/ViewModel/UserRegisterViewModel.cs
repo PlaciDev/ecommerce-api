aa438580cb77b7b123ebebd5c8c20e683ac44ff5
PlaciDev <brunoplacidev@gmail.com>
2025-07-22 21:13:33 -0300

    cria as ViewModels de login e registro de usuários

---
+++ EcommerceApi/ViewModel/UserRegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.ViewModel
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "O nome completo, deve conter entre 10 e 50 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O e-mail informado é inválido.")]
+        [StringLength(100, ErrorMessage = "O e-mail deve conter no máximo 100 caracteres.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [MinLength(12, ErrorMessage = "A senha deve conter no mínimo 12 caracteres.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$",
+            ErrorMessage = "A senha deve conter pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "A confirmação de senha é obrigatória.")]
+        [Compare("Password", ErrorMessage = "A confirmação de senha não confere com a senha informada.")]
+        public string PasswordConfirm { get; set; }
+
+    }
+}
