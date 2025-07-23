8796e5d15b7441f8ce2b64c1b5923334e7d3a528
PlaciDev <brunoplacidev@gmail.com>
2025-07-22 21:12:20 -0300

    adiciona métodos de registro e logn no controlador User

--- EcommerceApi/Controllers/UserController.cs
+++ EcommerceApi/Controllers/UserController.cs
@@ -1,14 +1,109 @@
 ﻿using EcommerceApi.Data;
 using EcommerceApi.Models;
+using EcommerceApi.Services;
 using EcommerceApi.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
 
 namespace EcommerceApi.Controllers
-{
+{   
+    [Authorize(Roles = "Admin")]
     public class UserController : ControllerBase
     {
+        [AllowAnonymous]
+        [HttpPost("api/users/register")]
+        public async Task<IActionResult> Register(
+            [FromServices] ApiDbContext context,
+            [FromBody] UserRegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var role = await context.Roles.FirstOrDefaultAsync(x => x.Name == "Default");
+                var user = new User
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    PasswordHash = PasswordHasher.Hash(model.Password),
+                    Role = role
+                };
+
+                if (user is null)
+                {
+                    return BadRequest("Dados inválidos...");
+                }
+
+                if (await context.Users.AnyAsync(x => x.Email == model.Email))
+                {
+                    return BadRequest("Já existe um usuário com este e-mail...");
+                }
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+
+                return Created($"api/users/{user.Id}", user);
+            }
+            catch
+            {
+                return StatusCode(500, "Erro interno no servidor...");
+          
+            }
+
+        }
+
+        [AllowAnonymous]
+        [HttpPost("api/users/login")]
+        public async Task<IActionResult> Login(
+            [FromServices] ApiDbContext context,
+            [FromServices] TokenService tokenService,
+            [FromBody] UserLoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await context
+                .Users
+                .AsNoTracking()
+                .Include(x => x.Role)
+                .FirstOrDefaultAsync(x => x.Email == model.Email);
+
+            if (user is null)
+            {
+                return NotFound("Usuário ou senha inválidos...");
+            }
+
+            if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+            {
+                return NotFound("Usuário ou senha inválidos...");
+            }
+
+            try
+            {
+                var token = tokenService.GenerateToken(user);
+                return Ok(token);
+            }
+            catch
+            {
+
+                return StatusCode(500, "Erro interno no servidor...");
+            }
+        }
+
+
+
+
+
+
+
+        
         [HttpGet("api/users")]
         public async Task<IActionResult> Get(
             [FromServices] ApiDbContext context)
@@ -48,6 +143,7 @@ namespace EcommerceApi.Controllers
             }
         }
 
+       
         [HttpGet("api/users/{id:int}")]
         public async Task<IActionResult> GetById(
             [FromServices] ApiDbContext context,
@@ -70,7 +166,7 @@ namespace EcommerceApi.Controllers
                 return StatusCode(500, "Erro interno no servidor...");
             }
         }
-
+        
         [HttpPost("api/users")]
         public async Task<IActionResult> Post(
             [FromServices] ApiDbContext context,
