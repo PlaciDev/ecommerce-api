916c92af7de29cf6cc03672326617cc090f14933
PlaciDev <brunoplacidev@gmail.com>
2025-07-22 21:12:54 -0300

    implementa o serviço de geração de token jwt

---
+++ EcommerceApi/Services/TokenService.cs
@@ -0,0 +1,37 @@
+﻿using EcommerceApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace EcommerceApi.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(ClaimTypes.Role, user.Role.Name)
+                }),
+
+                Expires = DateTime.UtcNow.AddHours(8),
+
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+                
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+        
+    }
+}
