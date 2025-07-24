7928d9de0ff9a0d25b5e77c26aed87a555113000
PlaciDev <brunoplacidev@gmail.com>
2025-07-23 23:37:46 -0300

    cria a ViewModel de ediçao para endereço

---
+++ EcommerceApi/ViewModel/AddressEditViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.ViewModel
+{
+    public class AddressEditViewModel
+    {
+        [Required(ErrorMessage = "A rua é obrigatória")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória")]
+
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "O estado é obrigatório")]
+
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "O CEP é obrigatório")]
+
+        public string ZipCode { get; set; }
+ 
+    }
+}
