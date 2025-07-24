2c3de432efd770ffe624be5d88f0df6a5b99de05
PlaciDev <brunoplacidev@gmail.com>
2025-07-23 23:37:12 -0300

    cria as ViewModels de login e registro do cliente

---
+++ EcommerceApi/ViewModel/CustomerLoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.ViewModel
+{
+    public class CustomerLoginViewModel
+    {
+        [Required(ErrorMessage = "Email é obrigatório.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatória.")]
+        public string Password { get; set; }
+    }
+}
---
+++ EcommerceApi/ViewModel/CustomerRegisteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceApi.Models;
+
+namespace EcommerceApi.ViewModel
+{
+    public class CustomerRegisteViewModel
+    {
+        public UserRegisterViewModel User { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public AddressEditViewModel Address { get; set; }
+    }
+}
