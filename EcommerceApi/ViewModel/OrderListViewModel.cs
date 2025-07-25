ff5d6e78933d625b6bea131f996929a7e8bd443f
PlaciDev <brunoplacidev@gmail.com>
2025-07-24 23:02:35 -0300

    cria o ViewModel de listagem, para Order

---
+++ EcommerceApi/ViewModel/OrderListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceApi.Models;
+
+namespace EcommerceApi.ViewModel
+{
+    public class OrderListViewModel
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public Payment Payment { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public enum OrderStatus
+        {
+            Pending,
+            Processing,
+            Shipped,
+            Delivered,
+            Cancelled
+        }
+    }
+}
