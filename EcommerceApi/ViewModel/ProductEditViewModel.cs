f9695d66bf77f0823381593b175ac2dc153a745a
PlaciDev <brunoplacidev@gmail.com>
2025-07-24 23:03:12 -0300

    cria o ViewModel de edição para Produc

---
+++ EcommerceApi/ViewModel/ProductEditViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.ViewModel
+{
+    public class ProductEditViewModel
+    {
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        [StringLength(50, ErrorMessage = "O nome do produto deve ter no máximo 50 caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "A descrição do produto é obrigatória.")]
+        [StringLength(100, ErrorMessage = "A descrição do produto deve ter no máximo 100 caracteres.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O preço do produto é obrigatório.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "O estoque do produto é obrigatório.")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "A categoria do produto é obrigatória.")]
+        public string CategoryName { get; set; }
+
+
+    }
+}
