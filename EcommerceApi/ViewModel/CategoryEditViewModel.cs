b6397b8921d71484b4669c93d0b4426d6dd30b2a
PlaciDev <brunoplacidev@gmail.com>
2025-07-24 23:01:32 -0300

    cria o ViewModel de edição para Category

---
+++ EcommerceApi/ViewModel/CategoryEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.ViewModel
+{
+    public class CategoryEditViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(20, ErrorMessage = "O nome não pode exceder 20 caracteres.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "A descrição é obrigatória.")]
+        [StringLength(100, ErrorMessage = "A descrição não pode exceder 100 caracteres.")]
+        public string Description { get; set; }
+    }
+}
