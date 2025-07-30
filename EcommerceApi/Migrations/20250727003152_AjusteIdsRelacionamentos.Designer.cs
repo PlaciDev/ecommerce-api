678c35d41f50d85708d54f6a7b7fc97f953dc8ff
PlaciDev <brunoplacidev@gmail.com>
2025-07-29 22:32:04 -0300

    Adiciona novas entidades e atualiza PhoneNumber

--- EcommerceApi/Migrations/20250727003152_AjusteIdsRelacionamentos.Designer.cs
+++ EcommerceApi/Migrations/20250727003152_AjusteIdsRelacionamentos.Designer.cs
@@ -4,14 +4,16 @@ using EcommerceApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EcommerceApi.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250727003152_AjusteIdsRelacionamentos")]
+    partial class AjusteIdsRelacionamentos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EcommerceApi/Migrations/20250727003152_AjusteIdsRelacionamentos.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceApi.Migrations
+{
+    public partial class AjusteIdsRelacionamentos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customer",
+                type: "VARCHAR(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customer",
+                type: "VARCHAR",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
